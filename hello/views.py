54e2f5a0f0b9145d7e9ac9127236e5d7556b69fb
NOOMA-42 <suncloud2203357667@gmail.com>
2018-09-28 11:30:58 +0800

    use template lang

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'hw1.html')
+    ttt = [0, 1, 2]
+    return render(request, 'hw1.html',locals())
