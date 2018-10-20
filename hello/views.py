4a6b7fd3ce6d18aac9520d293b229eb79e4d8034
NOOMA-42 <suncloud2203357667@gmail.com>
2018-10-20 09:44:55 +0800

    form functioning

--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, render
 from hello.models import game
-
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 def livestream(request):
     ttt = [0, 1, 2]
     if request.method == 'POST':
         #放輸入遊戲的地方 沒有
-        return redirect('/livestream') 
-    return render_to_response('livestream.html',locals())
+        return redirect('livestream/')
 
+    if request.method == 'GET':
+        return render(request, 'livestream.html', {'ttt': ttt,})
+
+#@csrf_protect
 def homepage(request):
-    listitem = game.objects.all()
-    return render_to_response('homepage.html', locals())
+    if request.method == 'POST':
+        name = request.POST.get('game')
+        game.objects.create(name = name)
+        listitem = game.objects.all()
+        return render(request, 'homepage.html', {"listitem": listitem,})
+    if request.method == 'GET':
+        listitem = game.objects.all()
+        return render(request, 'homepage.html', {"listitem": listitem,})
 
+    
